fc704451500606a0ca64ab867f8290e551ff42b1
zhengwujiang <cdzhengwujiang@jd.com>
2017-01-22 10:38:43 +0800

    调整UI，更传统的会话布局

--- pages/wd/wd.js
+++ pages/wd/wd.js
@@ -13,11 +13,23 @@ Page({
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
     // console.log(options);
+    // wx.clearStorage();
   },
   onShow: function(args) {
-    var data = this.data;
+    var data = this.data,
+        anim,
+        lastMsg;
+    anim = wx.createAnimation({
+      duration: 400,
+      timingFunction: 'ease-in-out'
+    });
+    this.anim = anim;
     data.user = wx.getStorageSync('qaList') || [];
+    lastMsg = data.user[data.user.length - 1];
     this.setData(data);
+    this.setData({
+      toView: 'toView' + (lastMsg ? lastMsg.id : 0)
+    });
   },
   toastChange: function() {
     var _data = this.data;
@@ -27,7 +39,10 @@ Page({
   formSubmit: function(e) {
     var _this = this,
         q = e.detail.value.question,
-        _data = _this.data;
+        _data = _this.data,
+        anim = this.anim;
+    anim.scale(1.1, 1.1).step();
+    anim.scale(1, 1).step();
     if (!q) {
       _data.toastHidden = false;
       _data.errMsg = '请输入问题';
@@ -38,6 +53,7 @@ Page({
     _data.hidden = true;
     _data.toastHidden = true;
     _data.errMsg = '';
+    _data.animationData = anim.export();
     _this.setData(_data);
     wx.request({
       url: api,
@@ -50,20 +66,25 @@ Page({
         'Content-Type': 'application/json'
       },
       success: function(res) {
-        var d;
+        var d, lastMsg;
         if (res.statusCode === 200) {
           d = res.data;
           _data.hidden = true;
-          _data.user.unshift({
+          lastMsg = _data.user[_data.user.length - 1];
+          _data.user.push({
+            id: lastMsg ? lastMsg.id + 1 : 1,
             question: "问：" + q,
             anwser: "答：" + (d.result.text || ''),
             url: d.result.url ? decodeURIComponent(d.result.url) : null
           });
           if (_data.user.length > 30) {
-            _data.user.length = 30;
+            _data.user = _data.user.slice(_data.user.length - 30);
           }
           wx.setStorageSync('qaList', _data.user);
           _this.setData(_data);
+          _this.setData({
+            toView: 'toView' + _data.user[_data.user.length - 1].id
+          });
         } else {
           _data.toastHidden = false;
           _data.errMsg = res.errMsg;
